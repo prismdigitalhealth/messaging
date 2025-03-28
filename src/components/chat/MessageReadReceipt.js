d0337f2602e01ecc05ce87dddf0b811f6e662cbd
Tuan Vu <prismdigitalhealth@gmail.com>
2025-03-28 12:08:30 -0700

    Change read receipt color to light grey

--- src/components/chat/MessageReadReceipt.js
+++ src/components/chat/MessageReadReceipt.js
@@ -72,11 +72,11 @@ const MessageReadReceipt = ({
       {(readStatus.isRead || forceShowRead) ? (
         <div className="read-status flex items-center" title={`Read by ${readStatus.readBy.map(u => getParticipantName(u.userId)).join(', ') || 'test user'}`}>
           <span className="read-icon mr-1">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3 h-3 text-blue-500">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-3 h-3 text-gray-400">
               <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
             </svg>
           </span>
-          <span className="font-medium text-blue-500">Read</span>
+          <span className="font-medium text-gray-400">Read</span>
           {(readStatus.readBy.length > 0 || forceShowRead) && (
             <span className="ml-1">
               {readStatus.readBy.length > 0 ? formatMessageTime(readStatus.readBy[0].readAt) : formatMessageTime(Date.now() - 60000)}
